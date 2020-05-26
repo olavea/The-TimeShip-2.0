e38638f49c438b155c85b9bc2e13f3946be07a68
Ola Vea <ola@olavea.com>
2020-05-26 09:58:56 +0200

    The TimeShip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <Helmet>
+        <title>The TimeShip</title>
+        <link href="/lillyicon3.png" rel="shortcut icon" type="image/x-icon" />
+      </Helmet>
+      <header></header>
+      <main>
+        <div>Hello world!</div>
+      </main>
+    </>
+  )
 }
