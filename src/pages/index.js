7468a6a7b8f0af8e4acd297bdff924833410499b
Ola Vea <ola@olavea.com>
2020-05-27 09:50:19 +0200

    Head elements by Gatsby.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 
 export default function Home() {
-  return <div>Hello Again world!</div>
+  return (
+    <>
+      <Helmet>
+        <title>The TimeShip</title>
+        <link href="/lillyicon3.png" rel="shortcut icon" type="image/x-icon" />
+      </Helmet>
+      <header></header>
+      <main>
+        <div id="auth-view">
+          <div>Hello Ruby Reckless!</div>
+        </div>
+      </main>
+    </>
+  )
 }
